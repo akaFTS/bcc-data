f0f92b49bc1619edb627d3f93f993414b6cb1272
akaFTS <gustavo84171@hotmail.com>
2018-11-25 21:02:48 -0200

    Add github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
+const openGithub = () => {
+  window.open('https://github.com/akaFTS/mac0499', '_blank')
+}
 
 const Header = () => {
   return (
@@ -10,6 +16,17 @@ const Header = () => {
         por <span className="fw6">Gustavo H F Silva</span> - orientador:
         <span className="fw6"> prof. Carlos Eduardo Ferreira</span>
       </p>
+      <div className="flex flex-row justify-center mv3">
+        <div
+          className="bg-gray hover-bg-mid-gray pointer white pv2 ph3 br-pill flex flex-row items-center"
+          onClick={openGithub}
+        >
+          <FontAwesomeIcon icon={faGithub} className="mr2 f4" />
+          <span className="fw3 f6">
+            Contribua no <span className="b">GitHub</span>
+          </span>
+        </div>
+      </div>
     </header>
   )
 }
