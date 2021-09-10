c013a080d21527552a32616cde988c177bba123e
Gustavo Silva <gustavohfts1@gmail.com>
2021-09-10 23:33:37 +0000

    add professors to course page

---
+++ components/course/professorCanvas.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  color: string;
+  icon: React.ReactNode;
+  size: number;
+  professorsList: any;
+};
+
+export default function ProfessorCanvas({
+  title,
+  color,
+  icon,
+  size,
+  professorsList,
+}: Props) {
+  return (
+    <article
+      className="br3 bg-light-gray mb4 overflow-hidden"
+      style={{
+        transition: "height 0.3s ease-out",
+        height: `${size * 50 + 50}px`,
+      }}
+      aria-owns={professorsList}
+    >
+      <header className={`bg-${color} white ph2 pv1 fw6 f6`}>
+        {icon}
+        <h3 className="ml2 di">{title}</h3>
+      </header>
+    </article>
+  );
+}
---
+++ data/course/professors.tsx
@@ -0,0 +1,336 @@
+import { Professor } from "~/types/course.tsx";
+
+export const professors: Professor[] = [
+  {
+    "code": "pf",
+    "name": "P. Feofiloff",
+  },
+  {
+    "code": "setzer",
+    "name": "V. Setzer",
+  },
+  {
+    "code": "simon",
+    "name": "Imre Simon",
+  },
+  {
+    "code": "istvan",
+    "name": "Istvan Simon",
+  },
+  {
+    "code": "silvio",
+    "name": "Silvio Ursic",
+  },
+  {
+    "code": "geraldo",
+    "name": "Geraldo Lino",
+  },
+  {
+    "code": "siang",
+    "name": "Siang",
+  },
+  {
+    "code": "routo",
+    "name": "Routo",
+  },
+  {
+    "code": "rene",
+    "name": "René",
+  },
+  {
+    "code": "waldo",
+    "name": "Waldo",
+  },
+  {
+    "code": "chumes",
+    "name": "C. Humes",
+  },
+  {
+    "code": "graca",
+    "name": "Graça Bressan",
+  },
+  {
+    "code": "zwicker",
+    "name": "R. Zwicker",
+  },
+  {
+    "code": "stolfi",
+    "name": "J. Stolfi",
+  },
+  {
+    "code": "tomasz",
+    "name": "Tomasz",
+  },
+  {
+    "code": "ines",
+    "name": "Inês H. de Mello",
+  },
+  {
+    "code": "edna",
+    "name": "Edna",
+  },
+  {
+    "code": "bete",
+    "name": "Bete Vivian",
+  },
+  {
+    "code": "anselmo",
+    "name": "Anselmo",
+  },
+  {
+    "code": "wagner",
+    "name": "Wagner",
+  },
+  {
+    "code": "miguel",
+    "name": "Miguel Haddad",
+  },
+  {
+    "code": "mandel",
+    "name": "Arnaldo",
+  },
+  {
+    "code": "yoshiko",
+    "name": "Yoshiko",
+  },
+  {
+    "code": "marcilio",
+    "name": "Marcílio",
+  },
+  {
+    "code": "nami",
+    "name": "Nami",
+  },
+  {
+    "code": "decio",
+    "name": "Décio",
+  },
+  {
+    "code": "maria-angela",
+    "name": "M. Ângela",
+  },
+  {
+    "code": "marfiza",
+    "name": "Marfiza",
+  },
+  {
+    "code": "stern",
+    "name": "J. Stern",
+  },
+  {
+    "code": "reverbel",
+    "name": "Reverbel",
+  },
+  {
+    "code": "afonso",
+    "name": "Afonso",
+  },
+  {
+    "code": "denise",
+    "name": "Denise",
+  },
+  {
+    "code": "ramos",
+    "name": "J. Ramos",
+  },
+  {
+    "code": "yoshi",
+    "name": "Yoshi",
+  },
+  {
+    "code": "alan",
+    "name": "Alan",
+  },
+  {
+    "code": "coelho",
+    "name": "Coelho",
+  },
+  {
+    "code": "carlinhos",
+    "name": "Carlinhos",
+  },
+  {
+    "code": "kunio",
+    "name": "Kunio",
+  },
+  {
+    "code": "dilma",
+    "name": "Dilma",
+  },
+  {
+    "code": "gubi",
+    "name": "Gubi",
+  },
+  {
+    "code": "pinhanez",
+    "name": "Pinhanez",
+  },
+  {
+    "code": "leucio",
+    "name": "Leucio",
+  },
+  {
+    "code": "arnaldo",
+    "name": "A. Vieira",
+  },
+  {
+    "code": "cris",
+    "name": "Cris",
+  },
+  {
+    "code": "tito",
+    "name": "Tito",
+  },
+  {
+    "code": "roberto-carlos",
+    "name": "R. Carlos",
+  },
+  {
+    "code": "daniel-panario",
+    "name": "D. Panario",
+  },
+  {
+    "code": "hae-yong",
+    "name": "Hae Yong",
+  },
+  {
+    "code": "alair",
+    "name": "Alair",
+  },
+  {
+    "code": "leonidas",
+    "name": "Leônidas",
+  },
+  {
+    "code": "ronaldo",
+    "name": "Ronaldo",
+  },
+  {
+    "code": "celma",
+    "name": "Celma",
+  },
+  {
+    "code": "barrera",
+    "name": "Junior",
+  },
+  {
+    "code": "pomeranz",
+    "name": "Pomeranz",
+  },
+  {
+    "code": "gold",
+    "name": "Alfredo",
+  },
+  {
+    "code": "kon",
+    "name": "Fabio Kon",
+  },
+  {
+    "code": "endler",
+    "name": "M. Endler",
+  },
+  {
+    "code": "paulo-geraldo",
+    "name": "P. Geraldo",
+  },
+  {
+    "code": "flavio",
+    "name": "Flávio",
+  },
+  {
+    "code": "acvm",
+    "name": "Ana Cristina",
+  },
+  {
+    "code": "finger",
+    "name": "M. Finger",
+  },
+  {
+    "code": "hernan",
+    "name": "Hernán",
+  },
+  {
+    "code": "hitoshi",
+    "name": "Hitoshi",
+  },
+  {
+    "code": "jef",
+    "name": "Jef",
+  },
+  {
+    "code": "marcondes",
+    "name": "Marcondes",
+  },
+  {
+    "code": "leliane",
+    "name": "Leliane",
+  },
+  {
+    "code": "ernesto",
+    "name": "Ernesto",
+  },
+  {
+    "code": "queiroz",
+    "name": "M. Queiroz",
+  },
+  {
+    "code": "nina",
+    "name": "Nina",
+  },
+  {
+    "code": "paulo-jose",
+    "name": "P. José",
+  },
+  {
+    "code": "renata",
+    "name": "Renata",
+  },
+  {
+    "code": "marcel",
+    "name": "Marcel",
+  },
+  {
+    "code": "hirata",
+    "name": "Hirata",
+  },
+  {
+    "code": "mascarenhas",
+    "name": "Walter",
+  },
+  {
+    "code": "gerosa",
+    "name": "Gerosa",
+  },
+  {
+    "code": "fujita",
+    "name": "Fujita",
+  },
+  {
+    "code": "daniel",
+    "name": "Daniel",
+  },
+  {
+    "code": "paulo-miranda",
+    "name": "P. Miranda",
+  },
+  {
+    "code": "kelly",
+    "name": "Kelly",
+  },
+  {
+    "code": "denis",
+    "name": "Denis",
+  },
+  {
+    "code": "marcel-k",
+    "name": "Marcel K.",
+  },
+  {
+    "code": "sinai",
+    "name": "Sinai",
+  },
+  {
+    "code": "fmario",
+    "name": "F. Mario",
+  },
+];
