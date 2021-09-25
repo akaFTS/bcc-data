92335522b8c98214dc85139882933c37e24d3032
Gustavo Silva <gustavohfts1@gmail.com>
2021-09-25 21:09:27 +0000

    Delete remaining JS assets

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import App from './App';
-
-render(<App />, document.querySelector('#app'));
