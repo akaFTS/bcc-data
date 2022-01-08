cb9454eb12e486c67b32577fa38cf9b535c47279
akaFTS <gustavohfts1@gmail.com>
2022-01-08 20:14:14 +0000

    Improve Web Vitals metrics

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600&subset=latin-ext&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
