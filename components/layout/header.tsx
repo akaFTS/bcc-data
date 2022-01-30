6369525e392bbb00546b97b2ecb513f041b10a90
akaFTS <gustavohfts1@gmail.com>
2022-01-30 04:11:36 +0000

    Add CSS modules to layout

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './header.module.css';
 
 export default function Header() {
   return (
-    <header className="pv3">
-      <h1 className="ma0 tc mt-dark-red f2">bcc-data</h1>
-      <p className="ma0 mb3 f4 tc silver fw4">A evolução do BCC em gráficos</p>
+    <header className={styles.header}>
+      <h1 className={styles.title}>bcc-data</h1>
+      <p className={styles.subtitle}>A evolução do BCC em gráficos</p>
     </header>
   );
 }
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -7,23 +7,41 @@ import {
   faUser,
 } from '@fortawesome/free-solid-svg-icons';
 import LinkButton from './linkButton';
+import styles from './navbar.module.css';
 
 export default function Navbar() {
   return (
-    <nav
-      aria-label="Navegação Principal"
-      className="flex flex-wrap justify-center justify-start-l mb4"
-    >
+    <nav aria-label="Navegação Principal" className={styles.nav}>
       <LinkButton
         text="Informações"
         icon={faFileAlt}
         path="/info"
-        color="orange"
+        color="orange-700"
       />
-      <LinkButton text="Curso" icon={faGraduationCap} path="/" color="purple" />
-      <LinkButton text="Matérias" icon={faBook} path="/classes" color="green" />
-      <LinkButton text="Grade" icon={faCalendarAlt} path="/grid" color="red" />
-      <LinkButton text="Alunos" icon={faUser} path="/students" color="blue" />
+      <LinkButton
+        text="Curso"
+        icon={faGraduationCap}
+        path="/"
+        color="purple-700"
+      />
+      <LinkButton
+        text="Matérias"
+        icon={faBook}
+        path="/classes"
+        color="green-700"
+      />
+      <LinkButton
+        text="Grade"
+        icon={faCalendarAlt}
+        path="/grid"
+        color="red-600"
+      />
+      <LinkButton
+        text="Alunos"
+        icon={faUser}
+        path="/students"
+        color="indigo-800"
+      />
     </nav>
   );
 }
