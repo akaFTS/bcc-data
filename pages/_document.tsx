1fe7e4949e8124e48bac9afd00c9c524036afaa2
akaFTS <gustavohfts1@gmail.com>
2022-02-15 01:24:54 +0000

    Bonus: add PWA support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,34 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#d32f2f" />
           <link
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600&subset=latin-ext&display=swap"
             rel="stylesheet"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/icons/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/icons/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon-16x16.png"
           />
         </Head>
         <body>
