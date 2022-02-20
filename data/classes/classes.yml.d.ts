1c63f5f190c311a6a68866a6a039a21016682413
akaFTS <gustavohfts1@gmail.com>
2022-02-20 03:13:50 +0000

    Add some more YAML files

---
+++ data/classes/classes.yml.d.ts
@@ -0,0 +1,4 @@
+import { Classe } from 'types/classes';
+
+const data: Classe[];
+export default data;
--- hooks/useGridPicker.ts
+++ hooks/useGridPicker.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Grid } from 'types/grid';
-import grids from 'data/grid/grids';
+import grids from 'data/grid/grids.yml';
 
 export default function useGridPicker(): [
   Grid[],
@@ -8,9 +8,12 @@ export default function useGridPicker(): [
   () => void,
   () => void,
 ] {
-  const [activeGridIndex, selectGrid] = useState(7);
-  const moveLeft = () => selectGrid((activeGridIndex + 7) % 8);
-  const moveRight = () => selectGrid((activeGridIndex + 9) % 8);
+  const numGrids = grids.length;
+  const [activeGridIndex, selectGrid] = useState(numGrids - 1);
+  const moveLeft = () =>
+    selectGrid((activeGridIndex + numGrids - 1) % numGrids);
+  const moveRight = () =>
+    selectGrid((activeGridIndex + numGrids + 1) % numGrids);
 
   return [grids, activeGridIndex, moveLeft, moveRight];
 }
