4d2cdb150fdaee7b7e5f25159a2e784eb26e089c
akaFTS <gustavohfts1@gmail.com>
2022-02-21 03:05:56 +0000

    Migrate professors to YAML

---
+++ data/course/professorLevels.yml.d.ts
@@ -0,0 +1,4 @@
+import { ProfessorYearEntry } from 'types/course';
+
+const data: ProfessorYearEntry[];
+export default data;
--- data/course/professors/1972.ts
+++
@@ -1,7 +0,0 @@
-import { ProfessorYearEntry } from 'types/course';
-
-export const yearDefinition: ProfessorYearEntry = {
-  MS1: ['geraldo', 'istvan', 'pf', 'routo', 'siang', 'silvio'],
-  MS2: ['simon'],
-  MS3: ['setzer'],
-};
